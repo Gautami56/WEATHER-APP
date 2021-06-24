f234bd55f58f96559e03327be92b3d6095d74808
Gautami56 <gautu.pinkyar@gmail.com>
2021-06-24 15:41:11 +0530

    Weather App Changes

--- src/WeatherComponent/index.jsx
+++ src/WeatherComponent/index.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
 import ReactTable from "react-table-6";
+import Moment from "moment";
 import getColor from "./getColorStyle";
 import configData from "../resources/config.json";
 
 function Weather() {
-  const [messages, setMessages] = useState([]);
+  const [qualityData, setQualityData] = useState([]);
+  const [wsResponse, setWsResponse] = useState([]);
   const webSocket = useRef(null);
 
   const columns = [
@@ -21,6 +23,7 @@ function Weather() {
       },
     },
     {
+      id: "aqi",
       Header: "AQI",
       accessor: "aqi",
       getProps: ({}, rowInfo, {}) => {
@@ -35,27 +38,72 @@ function Weather() {
       },
     },
     {
+      id: "updatedAt",
       Header: "LAST UPDATED",
-      accessor: "lastUpdatedDate",
+      accessor: (row) => {
+        return Moment(row.lastUpdateAt).format("h:mm:ss");
+      },
     },
   ];
+  useEffect(() => {
+    /**
+     * Step 1 - Take Fresh Data and check data is there - proper format
+     * Step 1.1 - If data is not proper, simply return
+     * Step 2  - Parse the data
+     * Step 3 - CleanUp data - decial format and addTimestampInUnix
+     * Step 4 - Check if data is not there in qualityData
+     * Step 4.1 - Set the data in qualityData and return
+     * Step 5 - Map through outer array - fresh data , map through inner array
+     * Step 5.1 - If no comparison of city - push the entire object inside the qualityData. Set qualityData
+     * Step 5.2 - qualityData.Perform comparison of City and update the
+     * aqi and update timestamp
+     * Step 5.3 - Update the state qualityData
+     *
+     */
+    //STEP 4
+    if (!qualityData || qualityData.length === 0) {
+      setQualityData(wsResponse);
+      return;
+    }
+
+    //STEP 5
+
+    wsResponse.map((w) => {
+      let index = qualityData.findIndex((item) => item.city === w.city);
+      if (index === -1) {
+        qualityData.push(w);
+      } else {
+        qualityData[index]["aqi"] = w.aqi;
+        qualityData[index]["lastUpdatedAt"] = w.lastUpdatedAt;
+      }
+    });
+    setQualityData(qualityData);
+  }, [wsResponse]);
 
   useEffect(() => {
     webSocket.current = new WebSocket(`${configData.WEB_SOCKET_SERVER_URL}`);
     webSocket.current.onmessage = (message) => {
-      let msg = JSON.parse(message.data);
-      msg.map((value) => {
+      //STEP 1
+      if (!message || !message.data) {
+        return null;
+      }
+      //STEP 2
+      const parsedData = JSON.parse(message.data);
+
+      //STEP 3
+      parsedData.map((value) => {
         value.aqi = Number(value.aqi).toFixed(`${configData.DECIMAL_SHIFT}`);
+        value.lastUpdateAt = new Date().getTime();
       });
-      setMessages(msg);
+      setWsResponse(parsedData);
+      return () => webSocket.current.close();
     };
-    return () => webSocket.current.close();
-  }, [messages]);
+  }, []);
 
   return (
-    <div>
-      <ReactTable data={messages} columns={columns} />
-    </div>
+    <React.Fragment>
+      <ReactTable data={qualityData} columns={columns} />
+    </React.Fragment>
   );
 }
 export default Weather;
